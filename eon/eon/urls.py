1fd36958cf0b184fddd73a2aef0f7d3274133f54
MathewsPK <143189387+MathewsPK@users.noreply.github.com>
2026-04-14 14:55:26 +0530

    Refactor urls.py for better import organization
    Moved TemplateView import to the top and adjusted urlpatterns.

--- eon/eon/urls.py
+++ eon/eon/urls.py
@@ -1,17 +1,17 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView  # move import here
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('robots.txt', TemplateView.as_view(
+        template_name='robots.txt',
+        content_type='text/plain'
+    )),
     path('', include('eonapp.urls')),
-    from django.views.generic import TemplateView
-
-path('robots.txt', TemplateView.as_view(
-    template_name='robots.txt',
-    content_type='text/plain'
-)),
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
