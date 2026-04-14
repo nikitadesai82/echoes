4b2cc26b07251a88c06cea4a62fda98e901c8ccb
MathewsPK <143189387+MathewsPK@users.noreply.github.com>
2026-04-14 14:54:59 +0530

    Add robots.txt route to urlpatterns
    Add a route for robots.txt using TemplateView

--- eon/eon/urls.py
+++ eon/eon/urls.py
@@ -6,6 +6,12 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('eonapp.urls')),
+    from django.views.generic import TemplateView
+
+path('robots.txt', TemplateView.as_view(
+    template_name='robots.txt',
+    content_type='text/plain'
+)),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
